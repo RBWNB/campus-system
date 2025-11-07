78451ffa25faf893dd3af2d810ddccde7358e0ee
FlnyXx <1287961978@qq.com>
2025-11-07 20:54:10 +0800

    注释修改

--- src/main/java/com/example/campus/config/SecurityConfig.java
+++ src/main/java/com/example/campus/config/SecurityConfig.java
@@ -18,12 +18,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private UserDetailsServiceImpl userDetailsService;
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder(){//密码加密器，加密方式使用 BCrypt
         return new BCryptPasswordEncoder();
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {//密码加密器，加密方式使用 BCrypt
         auth.userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder());
     }
