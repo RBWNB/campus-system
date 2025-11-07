fabcc1b2d3b8c9006ec4a08836be73398b39e4a9
liangguize2024 <m14771521020@163.com>
2025-11-07 21:14:08 +0800

    在学生端保存信息将名字name插入到user表中

---
+++ src/main/java/com/example/campus/dto/SaveDTO.java
@@ -0,0 +1,12 @@
+package com.example.campus.dto;
+import lombok.Data;
+
+@Data
+public class SaveDTO {
+    private String studentNo;
+    private String major;
+    private String grade;
+    private String phone;
+    private String address;
+    private String name;
+}
