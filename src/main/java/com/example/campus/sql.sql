ca730f681c4fb8ddfd3418698d6ad316e7b1b2c5
FlnyXx <1287961978@qq.com>
2025-11-08 23:51:53 +0800

    妈妈生的，兄弟你这sql有毒

--- src/main/java/com/example/campus/sql.sql
+++ src/main/java/com/example/campus/sql.sql
@@ -1,18 +1,5 @@
-CREATE DATABASE campus_system;
+create DATABASE campus_system;
 
 
--- 江，对管理员模块users表的约束
--- 姓名：2-4个汉字
-ALTER TABLE users
-    ADD CONSTRAINT chk_users_name CHECK (name REGEXP '^[\\u4e00-\\u9fa5]{2,4}$');
--- 邮箱：含@且@前1-20字符
-ALTER TABLE users
-    ADD CONSTRAINT chk_users_email CHECK (email REGEXP '^.{1,20}@.+$');
--- 密码：同时包含字母和数字
-ALTER TABLE users
-    ADD CONSTRAINT chk_users_password CHECK (password REGEXP '^(?=.*[a-zA-Z])(?=.*\\d).+$');
--- 用户名：最多16字符
-ALTER TABLE users
-    ADD CONSTRAINT chk_users_username CHECK (LENGTH(username) <= 16);
 
 
